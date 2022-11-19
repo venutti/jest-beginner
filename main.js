1a45df3c9e56357e580b54ac0bff8ff0e66a705f
venutti <lvenutti@fi.uba.ar>
2022-11-18 21:51:32 -0300

    Agrega la función ReverseString y sus tests

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 export const capitalize = function (string) {
   return string.slice(0, 1).toUpperCase() + string.slice(1);
 };
+export const reverseString = function (string) {
+  return string.split("").reverse().join("");
+};
--- main.test.js
+++ main.test.js
@@ -1,6 +1,6 @@
-import { capitalize } from "./main.js";
+import { capitalize, reverseString } from "./main.js";
 
-describe("function capitalize", () => {
+describe("function: capitalize", () => {
   it("the function capitalize the first char", () => {
     expect(capitalize("abc")).toBe("Abc");
   });
@@ -12,5 +12,17 @@ describe("function capitalize", () => {
   });
   it("the function doesnt do anything with number strings", () => {
     expect(capitalize("123")).toBe("123");
+  });
+});
+
+describe("function: reverse string", () => {
+  it("the function reverse all the chars of the string", () => {
+    expect(reverseString("abc")).toBe("cba");
+  });
+  it("the function works with empty strings", () => {
+    expect(reverseString("")).toBe("");
+  });
+  it("the function works with number strings", () => {
+    expect(reverseString("123")).toBe("321");
   });
 });
